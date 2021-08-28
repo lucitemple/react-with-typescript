f4869cb77bf85520c77efa9b9fc08353227ac3f7
Luci Temple <lucitemple@yahoo.com.au>
2021-08-28 10:51:19 +1000

    Add state & input fields to AddToList

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddToList = () => {
+const AddToList = (): JSX.Element => {
+  const [input, setInput] = useState({
+    name: "",
+    age: "",
+    note: "",
+    img: "",
+  });
 
-    return (
-        <div>
-            AddToList Component
-        </div>
-    )
-}
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <div className="AddToList">
+      <input
+        type="text"
+        placeholder="Name"
+        className="AddToList-input"
+        value={input.name}
+        onChange={handleChange}
+        name="name"
+      ></input>
+      <input
+        type="text"
+        placeholder="Age"
+        className="AddToList-input"
+        value={input.age}
+        onChange={handleChange}
+        name="age"
+      ></input>
+      <input
+        type="text"
+        placeholder="Image URL"
+        className="AddToList-input"
+        value={input.img}
+        onChange={handleChange}
+        name="img"
+      ></input>
+      <input
+        type="text"
+        placeholder="Notes"
+        className="AddToList-input"
+        value={input.note}
+        onChange={handleChange}
+      ></input>
+    </div>
+  );
+};
 
 export default AddToList;
